b390215970a252ac7b93c0d16d6b27ddcc50e542
Ali Siahkamari <ali.siahkamari@gmail.com>
2021-07-06 19:05:32 -0400

    Update PBDL.m

--- PBDL_code/PBDL.m
+++ PBDL_code/PBDL.m
@@ -4,7 +4,6 @@ function [bregman_div, params] = PBDL(y, X, m, lambda)
 % K = 100;
 % params = PBDLK_core(X, S1, S2, S3, S4, lambda, K);
 params = PBDL_core_triplets(X, S1, S2, S3, S4, lambda);
-params = PBDL_core_pairs_standard_form(y, X, lambda);
 % params = PBDL_core_pairs(y, X, lambda);
 bregman_div = @(X1,X2)max_affine_bregman(X1,X2,params, "all");
 
